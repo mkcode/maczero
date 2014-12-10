b2577f47994a424fda2a9068cf316184bb1a06ad
mkcode <chrisewald@gmail.com>
2014-12-10 12:00:58 +1100

    adding brew recipe

---
+++ recipes/brews.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: maczero
+# Recipe:: brews
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'homebrew::default'
+include_recipe 'homebrew::cask'
