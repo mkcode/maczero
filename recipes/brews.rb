d2ea7d1e12c11f07a9303116574ae68c200ee3b7
mkcode <chrisewald@gmail.com>
2015-01-02 17:46:31 +1100

    fix emacs install

--- recipes/brews.rb
+++ recipes/brews.rb
@@ -51,10 +51,17 @@ package 'emacs' do
   options '--cocoa --with-glib --with-gnutls --with-imagemagick --with-mailutils'
 end
 
+execute 'link emacs.app' do
+  cwd File.expand_path('~')
+  user node['current_user']
+  command "brew linkapps emacs"
+  not_if { File.exists? File.expand_path("~/Applications/Emacs.app") }
+end
+
 execute 'Loading emacs daemon at startup' do
   cwd File.expand_path('~')
   user node['current_user']
-  command "ln -sfv /usr/local/opt/emacs/*.plist ~/Library/LaunchAgents && launchctl load ~/Library/LaunchAgents/homebrew.mxcl.emacs.plist"
+  command "ln -sfv /usr/local/opt/emacs/*.plist ~/Library/LaunchAgents"
   not_if { File.exists? File.expand_path("~/Library/LaunchAgents/homebrew.mxcl.emacs.plist") }
 end
 
