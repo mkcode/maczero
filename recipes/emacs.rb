36ae8e0e87ad9270f6ad64e3942715275417c75a
mkcode <chrisewald@gmail.com>
2015-01-03 18:27:03 +1100

    adding emacs

--- recipes/emacs.rb
+++ recipes/emacs.rb
@@ -21,3 +21,18 @@ execute 'Loading emacs daemon at startup' do
   command "ln -sfv /usr/local/opt/emacs/*.plist ~/Library/LaunchAgents"
   not_if { File.exists? File.expand_path("~/Library/LaunchAgents/homebrew.mxcl.emacs.plist") }
 end
+
+git "get emacs repo from git" do
+  repository "https://github.com/mkcode/spacemacs"
+  destination File.join(node['projects_personal_dir'], "spacemacs")
+  revision 'master'
+  action :sync
+  enable_submodules true
+  user node['current_user']
+end
+
+link '~/.emacs.d' do
+  target_file File.expand_path("~/.emacs.d")
+  to File.join(node['projects_personal_dir'], "spacemacs")
+  user node['current_user']
+end
